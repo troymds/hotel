45d486ef5ad83ed3da37ca8ca9aa88c187770bd0
mads <mads@sina.com>
2014-12-09 08:45:07 +0800

    Initial Commit

---
+++ menuOrder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  menuOrder
+//
+//  Created by promo on 14-12-9.
+//  Copyright (c) 2014年 promo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
